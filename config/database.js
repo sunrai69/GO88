118dde6bfda37e58da3204eef47a99fc9b076c7c
Sun Rai <sunrai1542@gmail.com>
2024-10-23 21:07:22 +0700

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'url': process.env.MONGODB_URI || 'mongodb+srv://sunrai1542:<sunrai69>@cluster0.r8r1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+	'options': {
+		'dbName': 'CLUB3333', // red
+		//'dbName': 'GAME', // red
+		//'dbName': 'admin', // red
+                //'dbName': 'vn11022021', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
